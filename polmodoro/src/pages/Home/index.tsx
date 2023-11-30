e6636861b47e3f293e08063534449a7b45bf03f5
HENRY <113705213+Henrytos@users.noreply.github.com>
2023-11-30 09:46:19 -0300

    aidicionando logíca do coldown

--- polmodoro/src/pages/Home/index.tsx
+++ polmodoro/src/pages/Home/index.tsx
@@ -1,7 +1,15 @@
-import { InputHTMLAttributes, LabelHTMLAttributes, forwardRef } from "react";
-import { useForm } from "react-hook-form";
+import {
+  ButtonHTMLAttributes,
+  InputHTMLAttributes,
+  LabelHTMLAttributes,
+  forwardRef,
+  useEffect,
+  useState,
+} from "react";
+import { set, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from "zod";
+import { differenceInSeconds } from "date-fns";
 
 const TaskLabel = ({ ...props }: LabelHTMLAttributes<HTMLLabelElement>) => (
   <label className="font-bold" {...props} />
@@ -25,7 +33,9 @@ const TimeNumber = ({ ...props }) => (
   />
 );
 
-const ButtonInitialNewCycle = ({ ...props }) => (
+const ButtonInitialNewCycle = ({
+  ...props
+}: ButtonHTMLAttributes<HTMLButtonElement>) => (
   <button
     type="submit"
     className="bg-green-500/80 text-gray-50 flex h-16 
@@ -33,6 +43,18 @@ const ButtonInitialNewCycle = ({ ...props }) => (
     {...props}
   />
 );
+const ButtonFinalNewCycle = ({
+  ...props
+}: ButtonHTMLAttributes<HTMLButtonElement>) => {
+  return (
+    <button
+      type="button"
+      className="bg-red-500/80 text-gray-50 flex h-16 
+            justify-center items-center rounded-md disabled:bg-green-500/20 disabled:cursor-not-allowed disabled:text-gray-300 transition-colors"
+      {...props}
+    />
+  );
+};
 
 const newCicleFormValidarionScheme = zod.object({
   task: zod.string().min(1, "informe a tarefa"),
@@ -44,7 +66,65 @@ const newCicleFormValidarionScheme = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCicleFormValidarionScheme>;
 
+interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+  startDate: Date;
+  interruptedDate?: Date;
+}
+
 export default function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([]);
+  const [cycleActiveId, setCycleActiveId] = useState<string | null>(null);
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    const id = String(new Date().getTime());
+    const newCycle: Cycle = {
+      id,
+      minutesAmount: data.minutesAmount,
+      task: data.task,
+      startDate: new Date(),
+    };
+    setCycles((state) => [...state, newCycle]);
+    setCycleActiveId(id);
+    setAmountSecondsPassed(0);
+    reset();
+  }
+
+  const activeCycle = cycles.find((cycle) => cycle.id === cycleActiveId);
+
+  useEffect(() => {
+    let interval: number;
+    if (activeCycle) {
+      interval = setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate)
+        );
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [activeCycle]);
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = currentSeconds % 60;
+
+  const minutes = String(minutesAmount).padStart(2, "0");
+  const seconds = String(secondsAmount).padStart(2, "0");
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds} | Pomodoro`;
+    }
+  }, [activeCycle, seconds, minutes]);
+
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCicleFormValidarionScheme),
     defaultValues: {
@@ -53,10 +133,19 @@ export default function Home() {
     },
   });
 
-  function handleCreateNewCycle(data: NewCycleFormData) {
-    console.log(data);
-    reset();
+  function handleInterruptCycle() {
+    setCycles(
+      cycles.map((cycle) => {
+        if (cycle.id == cycleActiveId) {
+          return { ...cycle, interruptedDate: new Date() };
+        } else {
+          return cycle;
+        }
+      })
+    );
+    setCycleActiveId(null);
   }
+  console.log(cycles);
 
   const task = watch("task");
   const isSubmitDisabled = !task;
@@ -96,15 +185,21 @@ export default function Home() {
             <TaskLabel>minutos.</TaskLabel>
           </div>
           <div className="flex justify-center gap-4">
-            <TimeNumber>0</TimeNumber>
-            <TimeNumber>0</TimeNumber>
-            <span className="text-9xl text-green-500/80">:</span>
-            <TimeNumber>0</TimeNumber>
-            <TimeNumber>0</TimeNumber>
+            <TimeNumber>{minutes[0]}</TimeNumber>
+            <TimeNumber>{minutes[1]}</TimeNumber>
+            <span className="text-9xl text-green-500/80 font-bold">:</span>
+            <TimeNumber>{seconds[0]}</TimeNumber>
+            <TimeNumber>{seconds[1]}</TimeNumber>
           </div>
-          <ButtonInitialNewCycle disabled={isSubmitDisabled}>
-            Começar
-          </ButtonInitialNewCycle>
+          {activeCycle ? (
+            <ButtonFinalNewCycle onClick={() => handleInterruptCycle()}>
+              Parar
+            </ButtonFinalNewCycle>
+          ) : (
+            <ButtonInitialNewCycle disabled={isSubmitDisabled}>
+              Começar
+            </ButtonInitialNewCycle>
+          )}
         </form>
       </div>
     </>
