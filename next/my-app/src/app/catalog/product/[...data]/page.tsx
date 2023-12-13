34011ea05549e548563e504aaadf6272521a18a0
HENRY <113705213+Henrytos@users.noreply.github.com>
2023-12-13 16:55:44 -0300

    Add client-side code and header to layout, and fetch data in Home component

--- next/my-app/src/app/catalog/product/[...data]/page.tsx
+++ next/my-app/src/app/catalog/product/[...data]/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 interface ProductDetailsProps {
   params: {
     data: string[];
@@ -6,12 +7,19 @@ interface ProductDetailsProps {
 
 export default function productDetails(props: ProductDetailsProps) {
   const [id, color, quantity] = props.params.data;
+
+  console.log("add in cart");
+
+  function addInCart() {
+    console.log("add in cart");
+  }
   return (
     <main>
       <h1>productDetails</h1>
       <p>id: {id}</p>
       <p>color: {color}</p>
       <p>quantity: {quantity}</p>
+      <button onClick={addInCart}>add cart</button>
     </main>
   );
 }
--- next/my-app/src/app/page.tsx
+++ next/my-app/src/app/page.tsx
@@ -1,8 +1,12 @@
-export default function Home() {
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const data = await fetch("https://api.github.com/users/henrytos");
+  const response = await data.json();
   return (
     <main>
       {" "}
       <h1>home</h1>
+      <pre>{JSON.stringify(response, null, 2)}</pre>
     </main>
   );
 }
