e9b52bcc958acda4d7e32cc50b10ce8968101708
HENRY <113705213+Henrytos@users.noreply.github.com>
2023-12-18 15:07:42 -0300

    Update dependencies and styles

---
+++ desafios/coffe-delivery/src/app/home/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+  description: "catologo de café",
+};
+
+export default function HomeStore() {
+  return (
+    <main className="max-w-6xl m-auto">
+      <section id="home-initial">dfsf</section>
+    </main>
+  );
+}
---
+++ desafios/coffe-delivery/src/components/header.tsx
@@ -0,0 +1,27 @@
+import { MapPin, ShoppingCart } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header className="flex  max-w-6xl m-auto h-24 items-center justify-between bg-background">
+      <Link href="/home">
+        <Image src="/imgs/Logo.png" alt="logo tipo" width={85} height={40} />
+      </Link>
+
+      <nav className="flex gap-3">
+        <div className="flex gap-1 text-purple-dark bg-purple-light py-2.5 px-2  rounded-md ">
+          <MapPin />
+          <p>Porto Alegre, RS</p>
+        </div>
+
+        <Link
+          href="/cart"
+          className="p-2 rounded-md bg-yellow-light text-yellow-dark"
+        >
+          <ShoppingCart />
+        </Link>
+      </nav>
+    </header>
+  );
+}
