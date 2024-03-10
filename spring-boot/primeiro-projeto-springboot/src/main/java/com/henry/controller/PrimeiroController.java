ee31c83ae3429b577f007d6a825ee248022b8aa3
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-03-10 09:28:30 -0300

    diferentes tipos de params nos controllers

--- spring-boot/primeiro-projeto-springboot/src/main/java/com/henry/controller/PrimeiroController.java
+++ spring-boot/primeiro-projeto-springboot/src/main/java/com/henry/controller/PrimeiroController.java
@@ -1,15 +1,34 @@
 package com.henry.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/primeiraController")
 public class PrimeiroController {
 
-  @GetMapping("/primeiroMetodo")
-  public String primeiroMetodo() {
-    return "Primeiro método do controller";
+  @GetMapping("/primeiroMetodo/{id}")
+  public String primeiroMetodo(@PathVariable String id) {
+    return "Primeiro método do controller id=" + id;
+  }
+
+  @GetMapping("/pathParams/{id}")
+  public String pathParams(@PathVariable String id) {
+    return "pathParams id=" + id;
+  }
+
+  @GetMapping("/queryParams")
+  public String queryParams(@RequestParam String id) {
+    return "pathParams id=" + id;
+  }
+
+  @GetMapping("/queryParams2")
+  public String queryParams2(@RequestParam Map<String, String> allParams) {
+    return "todos os parametros são" + allParams.entrySet();
   }
 }
