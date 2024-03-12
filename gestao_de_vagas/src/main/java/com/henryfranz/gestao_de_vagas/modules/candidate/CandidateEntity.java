0a46dc477ca060df763793883b812176ce0d0b9b
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-03-12 20:42:58 -0300

    conectando com banco de dados pg

--- gestao_de_vagas/src/main/java/com/henryfranz/gestao_de_vagas/modules/candidate/CandidateEntity.java
+++ gestao_de_vagas/src/main/java/com/henryfranz/gestao_de_vagas/modules/candidate/CandidateEntity.java
@@ -1,8 +1,15 @@
 package com.henryfranz.gestao_de_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
@@ -10,12 +17,14 @@ import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
-
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
   @Pattern(regexp = "^(?!\\s*$).+", message = "o campo [userName] não pode ser vazio")
-  private String userName;
+  private String username;
 
   @Email(message = "o campo[email] deve conter um email vilido")
   @NotNull
@@ -25,5 +34,8 @@ public class CandidateEntity {
   private String password;
   private String description;
   private String curriculum;
+
+  @CreationTimestamp
+  private LocalDateTime cratedAt;
 
 }
