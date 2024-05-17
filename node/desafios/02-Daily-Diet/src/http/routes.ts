76b9f330464947eba78338ceb02fb62c04a964ca
henryFranz <166771474+PessoasDesaparecidas@users.noreply.github.com>
2024-05-17 02:03:17 -0300

    adicionando campo session_id

--- node/desafios/02-Daily-Diet/src/http/routes.ts
+++ node/desafios/02-Daily-Diet/src/http/routes.ts
@@ -1,8 +1,18 @@
+import { prisma } from "@/lib/prisma";
 import { FastifyInstance } from "fastify";
+import { z } from 'zod'
 
 export async function routesApp(app:FastifyInstance){
-    app.post('/',async (request,reply)=>{
-        console.log('hello')
-        return reply.status(201).send() 
+    app.post('/snack',async (request,reply)=>{
+        const bodyRequestSchema = z.object({
+            name: z.string(),
+            description:z.string(),
+            date_and_time:z.coerce.date(),
+            is_inside:z.boolean()
+        }) 
+        
+        const { name,description,date_and_time,is_inside} = bodyRequestSchema.parse(request.body)
+        
+        await prisma.
     })
 }
